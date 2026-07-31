63cb0acc6246ad3dc89ada63b52f0b8219a238dc
maira32 <mairatahir3@gmail.com>
2026-08-01 02:04:27 +0500

    feat: create User and Note data models

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-
 dotenv.config();
-
 const app = express();
-
+app.use(express.json());
 connectDB();
-
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
-
-const PORT = process.env.PORT || 5000;
-
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
