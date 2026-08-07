25fefbdead0b57a8c99f19e05b6172624b823b31
maira32 <mairatahir3@gmail.com>
2026-08-07 20:09:43 +0500

    feat: implement user and note API routes with JWT authentication

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
+import noteRoutes from './routes/noteRoutes.js';
+dotenv.config();
+const app = express();
+const PORT = 5000;
+app.use(express.json());
+app.use('/api/users', userRoutes);
+app.use('api/notes',noteRoutes);
+const startServer = async () => {
+  try {
+    await connectDB();
+        app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Failed to connect to the database:', error);
+    process.exit(1); 
+  }
+};
+startServer();
